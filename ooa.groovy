270a113a30a63e020c66d92adc4f12f630afba7a
Vamsikrishna Kalapala <135318320+vamsi1281977@users.noreply.github.com>
2024-08-07 13:07:07 +0530

    Create ooa.groovy

---
+++ ooa.groovy
@@ -0,0 +1,23 @@
+class ooa {
+  ooa(a, b, c) {
+    this.answer = this.solve(a, b, c)
+  }
+
+  def solve(a, b, c) {
+    def d = 0L
+    for (def e = 1; e < a; e = e + 1) {
+      def f = ((e % b) == 0)
+      def g = ((e % c) == 0)
+      def h = f || g
+      d = d + (h ? e : 0)
+    }
+    return d
+  }
+
+  def answer
+
+  static void main(args) {
+    def a = new ooa(1000, 3, 5)
+    println(a.answer)
+  }
+}

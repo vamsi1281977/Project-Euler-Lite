22556fa67e896dfdf4a9497730a1244b1e83776a
Vamsikrishna Kalapala <135318320+vamsi1281977@users.noreply.github.com>
2024-08-07 13:10:19 +0530

    Create oob.groovy

---
+++ oob.groovy
@@ -0,0 +1,26 @@
+class oob {
+  oob(a) {
+    this.answer = this.solve(a)
+  }
+
+  def solve(a) {
+    def b = 0
+    def c = 1
+    def d = 0
+    while (b <= a) {
+      def e = ((b % 2) == 0)
+      d = d + (e ? b : 0)
+      def f = b + c
+      b = c
+      c = f
+    }
+    return d
+  }
+  
+  def answer
+  
+  static void main(args) {
+    def a = new oob(4000000)
+    println(a.answer)
+  }
+}
